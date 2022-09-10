3e2f127325276288206078b4238b6b6fbec62dd7
EthanTPowell <ethantpowell00@gmail.com>
2022-09-10 13:24:16 -0400

    added schedule page

---
+++ schedule.js
@@ -0,0 +1,49 @@
+
+
+function grabMovies() {
+    let movies = localStorage.movies;
+}
+
+function grabRestaurants() {
+    let restaurants = JSON.parse(localStorage.restaurants);
+    console.log(restaurants);
+    rng(restaurants)
+
+}
+
+function rng(arr) {
+    const restaurant = arr[Math.floor(Math.random() * arr.length)];
+    console.log(restaurant);
+    displayRestaurant(restaurant);
+}
+
+function generateDate() {
+    console.log("generating");
+    grabRestaurants();
+
+}
+
+function displayRestaurant(restaurant) {
+    console.log(restaurant);
+    const mainBody = document.getElementById("mainBody");
+    mainBody.innerHTML += `<div id = "restDiv" class="container-md"></div>`
+    let restDiv = document.getElementById("restDiv");
+    let tempName = restaurant.poi.name;
+    let tempAdd = restaurant.address.freeformAddress
+    restDiv.innerHTML = 
+    `
+    <div class="card" id="${tempName}">
+    <h2>${tempName}</h2>
+    <p></p>
+    </div>
+    `
+    let tempCard = document.getElementById(`${tempName}`);
+    if (restaurant.poi.phone) {
+        tempCard.innerHTML += `<p>${restaurant.poi.phone}</p>`
+    }
+
+};
+
+function displayMovie() {
+    
+}
